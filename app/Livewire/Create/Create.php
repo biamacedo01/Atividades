40579f21e7f97a2c909ee67c8541cfab17b59ca9
biamacedo01 <bm681905@gmail.com>
2025-04-03 15:45:17 -0300

    commit inicial

--- app/Livewire/Create/Create.php
+++ app/Livewire/Create/Create.php
@@ -14,6 +14,33 @@ class Create extends Component
     public $email;
     public $password;
 
+
+
+    protected $rules=[
+        'nome'=>'required|nome|max:80',
+        'endereco'=>'required|endereco|min:10',
+        'telefone'=>'required|telefone|min:11',
+        'cpf'=>'required|cpf|min:11',
+        'email'=>'required|email|min:25',
+        'password'=>'required|password|min:6'
+        
+    ];
+     protected $message =[
+        'nome.required' => 'nome obrigatorio',
+        'nome.max' => 'nome deve conter no maximo 80 caracteres',
+        'endereco.required' => 'obrigatorio',
+        'endereco.min' => 'endereco  deve conter no minimo 10 caracteres',
+        'telefone.required' => 'telefone obrigatorio',
+        'telefone.required' => 'telefone deve conter no minimo 11 caracteres',
+        'cpf.required' => 'cpf obrigatorio',
+        'cpf.required' => 'cpf deve conter no minimo 11 caracteres',
+        'email.required' => 'nome obrigatorio',
+        'email.required' => 'email deve conter no minimo 25 carcateres',
+        'password.required'=>'password obrigatorio',
+        'password'=>'password deve conter no minimo 6 caracteres'
+
+     ];
+
     public function render()
     {
         return view('livewire.cliente.create');
